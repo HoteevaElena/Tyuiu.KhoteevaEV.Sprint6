26e9db8e9fbd2f8f610fe8a261214555b06e2c19
Tyuiu.HoteevaEV@outlook.com <Tyuiu.HoteevaEV@outlook.com>
2024-12-25 23:20:16 +0500

    Task7

---
+++ Tyuiu.KhoteevaEV.Sprint6.Task7.V16.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+
+namespace Tyuiu.KhoteevaEV.Sprint6.Task7.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
