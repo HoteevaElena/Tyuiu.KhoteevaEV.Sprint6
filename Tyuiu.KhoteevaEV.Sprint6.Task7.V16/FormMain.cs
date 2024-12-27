706743f4395b9b2d3baecf17d2e4567004c19ad8
Tyuiu.HoteevaEV@outlook.com <Tyuiu.HoteevaEV@outlook.com>
2024-12-27 18:00:47 +0500

    Task7

---
+++ Tyuiu.KhoteevaEV.Sprint6.Task7.V16/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.KhoteevaEV.Sprint6.Task7.V16
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
